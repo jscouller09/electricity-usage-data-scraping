336c8696434135d24d047f536f770406e76025bd
James Scouller <jscouller09@gmail.com>
2023-01-26 09:11:31 +1300

    updates to scraping script for Genesis website

--- compile_data.py
+++ compile_data.py
@@ -119,8 +119,8 @@ for ts, data in dups.iterrows():
     print('\t{:%Y-%m-%d %H:%M} | {}'.format(ts, data.usage))
 
 # billing period to check - note billing period will end at the end of the day on the last day
-bill_start = pd.to_datetime('10/01/2023', dayfirst=True) + pd.Timedelta(hours=1) # total for first hour of the billing period is at 1am
-bill_end = pd.to_datetime('19/01/2023', dayfirst=True) + pd.Timedelta(hours=24) # total for last hour of the billing period is at midnight on the day after
+bill_start = pd.to_datetime('20/01/2023', dayfirst=True) + pd.Timedelta(hours=1) # total for first hour of the billing period is at 1am
+bill_end = pd.to_datetime('19/02/2023', dayfirst=True) + pd.Timedelta(hours=24) # total for last hour of the billing period is at midnight on the day after
 bill_ts = all_data.loc[bill_start:bill_end].index
 bill_days = bill_ts[-1] - bill_ts[0]
 if bill_days.components.hours == 23:
--- scrape_data.py
+++ scrape_data.py
@@ -191,12 +191,10 @@ browser.login(continue_btn_id='continue', login_btn_id='next', username_fld_id='
 print('Navigating to usage page...')
 browser.click_button('div.header-tabs-top-link', hiding_elem_css='loading-portal', i=0)
 browser.click_button('a[href="/account/products/consumption"]', hiding_elem_css='loading-portal')
-
 # click the 3rd match for the button class, which is the hourly data button
-# browser.click_button('button.electricity-historical-tabs', i=2)
-# browser.click_button('button.electricity-historical-tabs', hiding_elem_css='loading-portal', i=2)
+browser.click_button('button.electricity-historical-tabs', hiding_elem_css='loading-portal', i=2)
 # extract data
-stop_date = pd.to_datetime('2023-01-21')
+stop_date = pd.to_datetime('2023-01-22')
 cur_date = datetime.now()
 while cur_date > stop_date:
     cur_date = browser.extract_data(toggle_btn_css='button.toggle', previous_btn_css='button.previous', no_data_css='div.error-text', data_css='div.chart-container.HOURLY.electricity-chart', download_btn_css='button.download-usage-excel')
