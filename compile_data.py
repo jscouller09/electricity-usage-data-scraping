78026b043311fccca98743a637458ae61134fa21
James Scouller <jscouller09@gmail.com>
2023-11-24 12:36:16 +1300

    Update compile_data.py

--- compile_data.py
+++ compile_data.py
@@ -120,8 +120,8 @@ if not dups.empty:
         print('\t{:%Y-%m-%d %H:%M} | {}'.format(ts, data.usage))
 
 # billing period to check - note billing period will end at the end of the day on the last day
-bill_start = pd.Timestamp(pd.to_datetime('18/10/2023', dayfirst=True), tz='Pacific/Auckland') # first day of billing period includes usage from 23:00-24:00 on the previous day
-bill_end = pd.Timestamp(pd.to_datetime('19/11/2023', dayfirst=True) + pd.Timedelta(hours=23), tz='Pacific/Auckland') # total for last hour of the billing period is at 23:00
+bill_start = pd.Timestamp(pd.to_datetime('20/11/2023', dayfirst=True), tz='Pacific/Auckland') # first day of billing period includes usage from 23:00-24:00 on the previous day
+bill_end = pd.Timestamp(pd.to_datetime('18/12/2023', dayfirst=True) + pd.Timedelta(hours=23), tz='Pacific/Auckland') # total for last hour of the billing period is at 23:00
 bill_ts = all_data.loc[bill_start:bill_end].index
 bill_days = bill_ts[-1] - bill_ts[0]
 if bill_days.components.hours == 23:
